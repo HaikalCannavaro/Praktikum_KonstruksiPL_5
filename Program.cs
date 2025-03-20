14b9db6bde36208b5683f08b56132a35a3b2df85
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2025-03-20 10:27:34 +0700

    menambahkan class PemrosesData dengan method

--- Program.cs
+++ Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class PemrosesData 
+{
+    public void DapatkanNilaiTerbesar<T>(T a, T  b, T c)
+    {
+        dynamic max = 0;
+        dynamic x = a;
+        dynamic y = b;
+        dynamic z = c;
+
+        if (x > y && x > z)
+        {
+            max = x;
+        }
+        else if (y > x && y > z)
+        {
+            max = y;
+        }
+        else if (z > x && z > y)
+        {
+            max = z;
+        }
+        else 
+        {
+            Console.WriteLine("Inputan tidak valid");
+        }
+        Console.WriteLine("Nilai terbesar = " + max);
+    }
+}
+
+class program
+{
+    static void Main() 
+    {
+        PemrosesData pemroses = new PemrosesData();
+        pemroses.DapatkanNilaiTerbesar(10, 30, 22);
+    }
+}
